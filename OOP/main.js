487dec1be17ff3ec38502f9bc029b4a5af852ce7
dzikrinasa <dzikrinandiasa@gmail.com>
2019-10-18 00:20:00 +0700

    up

--- OOP/main.js
+++ OOP/main.js
@@ -3,7 +3,14 @@
 var programer = {
     nama : "dzx",
     umur : 22,
-    sifat : "simple"
+    sifat : "simple",
+    //membuat metode 
+    getData : function(){
+        return "Nama : " + this.nama + " Umur : " + this.umur;
+    },
+    setData : function(nama){
+        this.nama = nama;
+    }
 };
 
 let disain = new Object();
@@ -13,3 +20,26 @@ disain.umur = 12;
 document.getElementById('nama').innerHTML = programer.nama;
 document.getElementById('umur').innerHTML = disain["umur"];
 document.getElementById('sifat').innerHTML = programer.sifat;
+
+programer.setData('dzz');
+document.getElementById('box').innerHTML = programer.getData();
+
+//Objek Prototype
+function pemain( nama, umur){
+    this.nama = nama;
+    this.umur = umur;
+}
+
+//menambahkan property diluar fungsi
+pemain.prototype.negara = "indo";
+
+var aku = new pemain("Aku prototyep", 33);
+var ika = new pemain("Ika prototyep", 33);
+document.getElementById('pro').innerHTML = aku.nama;
+document.getElementById('pro').innerHTML += ika.negara;
+
+//mutable
+let kuli = programer;
+kuli.nama = " messi";
+
+document.getElementById('pro').innerHTML += kuli.nama;
